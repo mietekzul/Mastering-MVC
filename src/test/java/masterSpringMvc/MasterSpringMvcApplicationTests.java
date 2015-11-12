ac1ac327365b1c57c97e7cee511c3bf0b6dfa26e
unknown <dlok@PCLLE105.gft.com>
2015-11-12 17:25:13 +0100

    Pierwszy STS

---
+++ src/test/java/masterSpringMvc/MasterSpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package masterSpringMvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MasterSpringMvcApplication.class)
+public class MasterSpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
