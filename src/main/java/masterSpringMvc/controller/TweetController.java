d6e5427e705c4658db1af0f73b511ecfba20cbc5
Ostapiuk, Daniel <daniel.ostapiuk@gft.com>
2015-11-19 17:26:30 +0100

    Added redirect

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -2,6 +2,8 @@ package masterSpringMvc.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -9,16 +11,18 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
 
 	@Autowired
 	private Twitter twitter;
-	
+
 	@RequestMapping("/")
-	public String home(){
+	public String home() {
 		return "searchPage";
 	}
 
@@ -31,5 +35,16 @@ public class TweetController {
 		model.addAttribute("search", search);
 
 		return "resultPage";
+	}
+
+	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String search = request.getParameter("search");
+		if (search.toLowerCase().contains("struts")) {
+			redirectAttributes.addFlashAttribute("error", "Try using spring instead");
+			return "redirect:/";
+		}
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:result";
 	}
 }
